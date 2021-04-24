7a1d013df2be552f2009fdf2557a516ed37f3523
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-04-24 17:10:11 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,10 +1,28 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 ////////
-
+/*
 client.on('ready', () => {
     client.user.setActivity(`Shtak Lera Bnusa`,{ type: 'STREAMING' });
-})
+})*/
 
+client.on("ready", () => {
+  console.log(
+    `Online In  on ${client.guilds.cache.size}`
+  );
+  let statuses = [
+    `💔🙂زۆرم خۆش ویستی بەڵام قەدرت نەزانیم`,
+    ` کە بەڵێنی مانەوەیان دا دڵنیابە دەڕۆن💔😀`,
+  ` دەبێت ڕۆشتنێک بڕۆم هەواڵی مردنم بکەم بە دیاری بۆت `,
+      ` kurd.gg خاوەنی`
+  ];
+  setInterval(function() {
+    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
+    client.user.setActivity(STREAMING, {
+      type: "STREAMING",
+      url: "http://www.twitch.tv/faith"
+    });
+  }, 1000);
+});
 //////
-client.login("token");
+client.login("");
