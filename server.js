3d1d968683e7b3ab2e708fe425247b371fff8c31
MeTaSploit <76389685+zallm00@users.noreply.github.com>
2021-04-24 17:01:36 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,18 +1,6 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://name.glitch.me/`);
-}, 280000);
-
-////////
 const Discord = require('discord.js');
 const client = new Discord.Client();
-client.on('error', console.error);
+////////
 
 client.on('ready', () => {
     client.user.setActivity(`Shtak Lera Bnusa`,{ type: 'STREAMING' });
