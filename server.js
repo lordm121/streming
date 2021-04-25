a4d1089d571500c8d7e38ac03f8a982b1de69773
lordm121 <74502300+lordm121@users.noreply.github.com>
2021-04-26 02:39:07 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 ////////
-/*
-client.on('ready', () => {
-    client.user.setActivity(`Shtak Lera Bnusa`,{ type: 'STREAMING' });
-})*/
 
-client.on("ready", () => {
-  console.log(
-    `bot is online`
-  );
-  let statuses = [
-    `💔🙂زۆرم خۆش ویستی بەڵام قەدرت نەزانیم`,
-    `کە بەڵێنی مانەوەیان دا دڵنیابە دەڕۆن💔😀`,
-  `دەبێت ڕۆشتنێک بڕۆم هەواڵی مردنم بکەم بە دیاری بۆت`,
-      `مkurd.gg خاوەنی`,
-`🙂من لە ناسینی تۆ پەشیمان نیم`,
-`💔😀 ئەگەر خۆشت ئەوێت دەی بیسەلمێنە بۆی`, 
-`💔😀باوەڕناکەم هێندەی من ئاشقی خۆت بیت`,
-`یەکەمین بۆت لیستی کوردی دروستکراوی خۆمە نامە بنێرن`,
-`iam kurdish devloper🎰🛠️`
-  ];
-  setInterval(function() {
-    let STREAMING = statuses[Math.floor(Math.random() * statuses.length)];
-    client.user.setActivity(STREAMING, {
-      type: "STREAMING",
-      url: "http://www.twitch.tv/faith"
-    });
-  }, 3000);
-});
+client.on('ready', () => {
+    client.user.setActivity(`Shtak Lera Bnusa`,{ type: 'PLAYING' });
+})
+
 //////
-client.login("Mzg2MTg4NDkxOTUzNzk5MTc4.YIQnmQ.Vf5DfUMq96bLf86oNeTGW5TQTn4");
+client.login("token");
